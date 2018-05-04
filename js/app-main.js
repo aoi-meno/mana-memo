ddc5fc5dd04889e5e342ce9bbf38f4254ac51dbf
hisakami-yuki <35251392+hisakami-yuki@users.noreply.github.com>
2018-05-05 00:01:49 +0900

    Update app-main.js

--- js/app-main.js
+++ js/app-main.js
@@ -2,26 +2,24 @@
 //-----------------------------------------------------------------------
 //tokenizer初期化
 var tokenizer = null;
-
 //基本データ初期化
 var data = {
-  mTitle:"",
+  mTitle: "",
   mText: "",
   tokens: [],
-  tokens_ex:[],
-  token_exs:[],
-  filter_basic:"",
-  filter_pos:"名詞",
+  tokens_ex: [],
+  token_exs: [],
+  filter_basic: "",
+  filter_pos: "名詞",
   filter_length: 1,
-  memoList:[],
+  memoList: [],
   isLoading: true,
   message: "機能をロードしています ...",
-  button_label_a:'Wikipedia',
-  button_label_b:'goo辞書',
+  button_label_a: 'Wikipedia',
+  button_label_b: 'goo辞書',
   setting_dic_name: '指定検索',
-  setting_dic_url:"https://ja.m.wikipedia.org/w/index.php?search=",
+  setting_dic_url: "https://ja.m.wikipedia.org/w/index.php?search=",
 };
-
 //function
 //-----------------------------------------------------------------------
 // 入力された内容をローカルストレージに保存する
@@ -35,18 +33,16 @@ function saveText() {
   data.mText = "";
   data.tokens = [];
 }
-
 // ローカルストレージに保存した値を再描画する
 function showText() {
   var key, value, html = [];
   data.memoList = [];
-  for(var i=0, len=localStorage.length; i<len; i++) {
+  for (var i = 0, len = localStorage.length; i < len; i++) {
     key = localStorage.key(i);
     value = localStorage.getItem(key);
     data.memoList.push(value);
   }
 }
-
 // 文字をエスケープする
 function escapeText(text) {
   var TABLE_FOR_ESCAPE_HTML = {
@@ -55,17 +51,16 @@ function escapeText(text) {
     "<": "&lt;",
     ">": "&gt;"
   };
-  return text.replace(/[&"<>]/g, function(match) {
+  return text.replace(/[&"<>]/g, function (match) {
     return TABLE_FOR_ESCAPE_HTML[match];
   });
 }
-
 // 入力チェックを行う
 function checkText(text) {
   if (0 === text.length || 20 < text.length) {
     alert("文字数は1〜20字にしてください");
     return false;
-    }
+  }
   var length = localStorage.length;
   for (var i = 0; i < length; i++) {
     var key = localStorage.key(i);
@@ -77,122 +72,104 @@ function checkText(text) {
   }
   return true;
 }
-
 //記号フィルタリング
 function validateString(val) {
   var reg = new RegExp(/[!"#$%&'()\*\+\-\.,、。「」\/:;<=>?@\[\\\]^_`{|}~]/g);
-	if(reg.test(val)) {
-		return false;
-	}
+  if (reg.test(val)) {
+    return false;
+  }
   return true;
 }
-
 //分析内容のフィルタリング機能
 function tokens_extract() {
-    for (var i = 0; i < data.tokens.length; i++) {
-    　if(data.tokens[i].pos == data.filter_pos && data.tokens[i].basic_form.length >= data.filter_length){
-			if(validateString(data.tokens[i].surface_form)){
-				data.tokens_ex.push(data.tokens[i].surface_form);
-			}
-    　}
-　}
-	//	重複削除
-	data.token_exs = data.tokens_ex.filter(function (x, i, self) {
-            return self.indexOf(x) === i;
-    });
+  for (var i = 0; i < data.tokens.length; i++) {　
+    if (data.tokens[i].pos == data.filter_pos && data.tokens[i].basic_form.length >= data.filter_length) {
+      if (validateString(data.tokens[i].surface_form)) {
+        data.tokens_ex.push(data.tokens[i].surface_form);
+      }　
+    }　
+  }
+  //	重複削除
+  data.token_exs = data.tokens_ex.filter(function (x, i, self) {
+    return self.indexOf(x) === i;
+  });
 }
-
-
-
-
-
-
-
 //vue and tokenize
 //-----------------------------------------------------------------------
 //各ページ内容,読み込み時の操作
 //Section1
 const loginPage = {
   template: '#login',
-  data: function() {
-  },
+  data: function () {},
 };
-
 //Section2
 const editerPage = {
   template: '#editer',
-  data: function() {
+  data: function () {
     return data;
   },
 };
 const analysisPage = {
   template: '#analysis',
-  data: function() {
+  data: function () {
     return data;
   },
 };
-
 //Section3
 const memolistPage = {
   template: '#mlist',
-  data: function() {
+  data: function () {
     return data;
   },
 };
 const login_memolistPage = {
   template: '#mlist_login',
-  data: function() {
+  data: function () {
     return data;
   },
 };
 const SearchPage = {
   template: '#search',
-  data: function() {
+  data: function () {
     return data;
   },
 };
 const settingsPage = {
   template: '#settings',
-  data: function() {
+  data: function () {
     return data;
   },
 };
 const settingsPage2 = {
   template: '#settings2',
-  data: function() {
+  data: function () {
     return data;
   },
 };
-
 //Vueメイン関数
 var main = new Vue({
   el: '#app',
   template: '#main',
-  data: function(){
+  data: function () {
     return {
-      activeIndex:0,
-      tabs: [
-        {
-          label: 'メモ登録',
-          page: editerPage,
-          key: "editerPage"
-        },
-        {
-          label: '分析',
-          page: analysisPage,
-          key: "analysisPage"
-        },
-        {
-          label: 'メモ一覧',
-          page: memolistPage,
-          key: "memolistPage"
-        },
-        {
-          label: '設定',
-          page: settingsPage,
-          key: "settingsPage"
-        }
-      ]
+      activeIndex: 0,
+      tabs: [{
+        label: 'メモ登録',
+        page: editerPage,
+        key: "editerPage"
+      }, {
+        label: '分析',
+        page: analysisPage,
+        key: "analysisPage"
+      }, {
+        label: 'メモ一覧',
+        page: memolistPage,
+        key: "memolistPage"
+      }, {
+        label: '設定',
+        page: settingsPage,
+        key: "settingsPage"
+      }]
     };
   },
   methods: {
@@ -208,54 +185,48 @@ var main = new Vue({
         data.tokens = [];
       }
     },
-    md: function(){
+    md: function () {
       return this.$ons.platform.isAndroid();
     },
   },
   computed: {
-    title: function(){
+    title: function () {
       return this.tabs[this.activeIndex].label;
     },
   },
 });
-
 //フォームの内容の変化を検知、変更されたらtokenizeする
-jQuery(document).ready(function($){
-    $("[id^=text_input]").bind('keyup change', function() {
-	    main.tokenize();
-        data.tokens_ex = [];
-        tokens_extract();
-	});
+jQuery(document).ready(function ($) {
+  $("[id^=text_input]").bind('keyup change', function () {
+    main.tokenize();
+    data.tokens_ex = [];
+    tokens_extract();
+  });
 });
-
-
 // リセットボタン押されたらメモ・分析内容をリセット
-document.querySelector("#clear_button").onclick = function() {
+document.querySelector("#clear_button").onclick = function () {
     data.mText = "";
-    data.tokens =[];
-	data.tokens_ex = [];
-	data.token_exs = [];
-},
-
-// 登録ボタン押されたらDB or 一覧に追加
-document.querySelector("#send_button").onclick = function() {
+    data.tokens = [];
+    data.tokens_ex = [];
+    data.token_exs = [];
+  },
+  // 登録ボタン押されたらDB or 一覧に追加
+  document.querySelector("#send_button").onclick = function () {
     saveText();
-},
-
-
-// Load and prepare tokenizer
-kuromoji.builder({ dicPath: "./dict/" }).build(function (error, _tokenizer) {
-  if (error != null) {
-    console.log(error);
-  }
-  tokenizer = _tokenizer;
-  data.message = "Ready";
-  data.mText = "";
-  data.isLoading = false;
-  //デバッグ
-  //console.log(data.tokens);
-  //console.log(data.isLoading);
-  //console.log(data.inputText);
-});
-
-
+  },
+  // Load and prepare tokenizer
+  kuromoji.builder({
+    dicPath: "./dict/"
+  }).build(function (error, _tokenizer) {
+    if (error != null) {
+      console.log(error);
+    }
+    tokenizer = _tokenizer;
+    data.message = "Ready";
+    data.mText = "";
+    data.isLoading = false;
+    //デバッグ
+    //console.log(data.tokens);
+    //console.log(data.isLoading);
+    //console.log(data.inputText);
+  });
